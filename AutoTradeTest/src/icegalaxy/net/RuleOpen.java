d5bd8576057e1cc2cea6074937f237a76070dfc4
IceGalaxy@MacAir <joecheung0@gmail.com>
2016-11-24 00:18:46 +0800

    Trade ruleOpen also in the afternoon, 567profit

--- AutoTradeTest/src/icegalaxy/net/RuleOpen.java
+++ AutoTradeTest/src/icegalaxy/net/RuleOpen.java
@@ -19,7 +19,7 @@ public class RuleOpen extends Rules
 	public RuleOpen(WaitAndNotify wan1, WaitAndNotify wan2, boolean globalRunRule)
 	{
 		super(wan1, wan2, globalRunRule);
-		setOrderTime(93000, 115500, 160000, 160000, 231500, 231500);
+		setOrderTime(93000, 115500, 130500, 160000, 231500, 231500);
 		chasing = new Chasing();
 		// wait for EMA6, that's why 0945
 	}
